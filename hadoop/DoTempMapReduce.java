0594c7c32a18953b8df201905fb2d9290e94afbf
YuWen <31069535+twilighthook@users.noreply.github.com>
2019-02-24 19:39:22 +0800

    Add files via upload

--- hadoop/DoTempMapReduce.java
+++ hadoop/DoTempMapReduce.java
@@ -1,5 +1,35 @@
 package com.test.hadoop;
 
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class DoTempMapReduce {
+	
+	public static void main(String[] args) throws Exception{
+		if(args.length != 2) {
+			System.out.println("");
+			System.exit(-1);
+		}
+		
+		Job job = Job.getInstance();
+		job.setJarByClass(DoTempMapReduce.class);
+		job.setJobName("Max Temperature");
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		job.setMapperClass(DoTempMapper.class);
+		job.setReducerClass(DoTempReducer.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		
+	}
 
 }
